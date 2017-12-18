ade63ddbc3f367c88457c9db8d6f7994254ba025
Michael Cook <michael.cook@techdept.co.uk>
2017-12-18 10:28:46 +0000

    Smashing lodash into a simple react app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Lodash Research</h1>
+        </header>
+        <main>Boom</main>
+      </div>
+    );
+  }
+}
+
+export default App;
