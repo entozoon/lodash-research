e67ef2c54621b6dacf08a57aefa6c53fa7a00469
Michael Cook <michael.cook@techdept.co.uk>
2017-12-18 10:33:00 +0000

    It works, so I guess.. I mean, now I have to actually _learn_ things. o.o

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
+var _ = require("lodash");
 
 class App extends Component {
+  constructor() {
+    super();
+
+    // Let's get lodashin'
+    let test1 = _.defaults({ a: 1 }, { a: 3, b: 2 });
+    console.log(test1); // → { 'a': 1, 'b': 2 }
+    let test2 = _.partition([1, 2, 3, 4], n => n % 2);
+    console.log(test2); // → [[1, 3], [2, 4]]
+  }
   render() {
     return (
       <div className="App">
